5c369abe43133c19a8e6c9286467fccf6f6256c9
Arpit <gautamarpit1204@gmail.com>
2023-12-11 21:59:14 +0530

    updated navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 
 function App() {
   return (
-    <div className="h-screen bg-gradient-to-b from-black to-gray-900">
+    <div className="h-screen bg-gradient-to-b from-black to-gray-500">
       <Router>
         <Navbar />
         <Routes>
